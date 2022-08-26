154788bebc7d1d15d144980b94bb3e11c4028296
avedyer <avedyer@gmail.com>
2022-08-26 10:58:44 -0400

    first commit

---
+++ src/about.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return(
+    <main>
+      <h2>About me</h2>
+      <p>
+        Hi! My name is Avery, and I'm a self taught web developer. My focus as a developer is creating websites with a user experience that is intuitive, attractive, and accesible.
+      </p>
+      <p>
+        My work primarily utilizes Javascript, HTML, CSS, and React.
+      </p>
+    </main>
+  )
+}
---
+++ src/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return(
+    <header>
+      <img />
+      <h1><span id='name'>Avery Dyer</span> is a web developer from Philadelphia</h1>
+    </header>
+  )
+}
